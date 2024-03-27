d2d47b33b274917657c7ea3729210f1984c55e5e
Lien Tran <tranthilien.m@gmail.com>
2024-03-27 14:01:31 +0700

    add navigation projetcs in Portfolio

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../../components/Navbar";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Portfolio() {
   const title = "Lien Tran's Portfolio";
@@ -8,10 +9,36 @@ export default function Portfolio() {
     <div className="App">
       <title>{title}</title>
 
-      <Image className="bg-image" src="/star.png" width={0} height={0} sizes="100vw" alt="bg-image" />
+      <Image
+        className="bg-image"
+        src="/star.png"
+        width={0}
+        height={0}
+        sizes="100vw"
+        alt="bg-image"
+      />
       <Navbar />
-
-      <h1>{title}</h1>
+      <main>
+        <h1>Portfolio</h1>
+        <ul>
+          <li>
+            <Link
+              href="https://lientran1990.github.io/net-gross-calculation/"
+              target="blank"
+            >
+              Net-Gross Calculator
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="https://lientran1990.github.io/portfolio-reactjs/"
+              target="blank"
+            >
+              Project Landing Page
+            </Link>
+          </li>
+        </ul>
+      </main>
     </div>
   );
 }
