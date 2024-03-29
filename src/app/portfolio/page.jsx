50cc6915f0dde2236cea46a12c01941cc2caed50
Lien Tran <tranthilien.m@gmail.com>
2024-03-29 15:48:30 +0700

    add Card component

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import Navbar from "../../components/Navbar";
 import Image from "next/image";
 import Link from "next/link";
+import "../globals.css";
+import "./page.css";
+import Card from "@/components/Card";
 
 export default function Portfolio() {
   const title = "Lien Tran's Portfolio";
@@ -19,8 +22,11 @@ export default function Portfolio() {
       />
       <Navbar />
       <main>
-        <h1>Portfolio</h1>
-        <ul>
+        <div className="main-content">
+          <p>Portfolio</p>
+          <hr width="30%;" color="orange" size="3"></hr>
+        </div>
+        <ul className="flex">
           <li>
             <Link
               href="https://lientran1990.github.io/net-gross-calculation/"
@@ -38,6 +44,7 @@ export default function Portfolio() {
             </Link>
           </li>
         </ul>
+        <Card />
       </main>
     </div>
   );
---
+++ src/components/Card.js
@@ -0,0 +1,9 @@
+export default function Card() {
+  const title = "Calculator";
+  return (
+    <div className="card-content">
+      <img src="salary-calculator-1.png" width={300} height={150} />
+      <p style={{ fontSize: 16 }}>{title}</p>
+    </div>
+  );
+}
